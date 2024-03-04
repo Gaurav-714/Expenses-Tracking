7bdc0cfb06ce40702dc8b0221720f7ea6194877f
Gaurav-714 <gauravnagose714@gmail.com>
2024-03-05 01:52:07 +0530

    Working

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ExpenseSerializer
+from .models import Expense
 
-# Create your views here.
+
+class TrackExpense(APIView):
+
+    def post(self, request):
+        title = request.data.get('title')
+        amount = request.data.get('amount')
+        #obj = Expense(user=request.user, title=title, expense=expense)
+        obj = Expense(title=title, amount=amount)
+        obj.save()
+
+        #expenses = request.objects.filter(user = request.user).order_by('date')
+        expenses = Expense.objects.all().order_by('date')
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response({
+            'status' : '200',
+            'data' : serializer.data
+        })        
+    
+    def get(self, request):
+        #expenses = request.objects.filter(user = request.user).order_by('date')
+        expenses = Expense.objects.all().order_by('date')
+        serializer = ExpenseSerializer(expenses)
+        return Response({
+            'status' : '200',
+            'data' : serializer.data
+        }) 
