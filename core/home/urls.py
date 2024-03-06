6649b06924ef576045ed4fdbe080a699c1932694
Gaurav-714 <gauravnagose714@gmail.com>
2024-03-06 18:41:35 +0530

    user authentication

--- core/home/urls.py
+++ core/home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', TrackExpense.as_view())
+    path('expenses/', TrackExpense.as_view()),
+    path('signup/', SignUpView.as_view(), name = 'signup'),
+    path('signin/', SignInView.as_view(), name = 'signin'),
+    path('signout/', SignOutView.as_view(), name = 'signout'),
 ]
--- core/home/views.py
+++ core/home/views.py
@@ -1,32 +1,102 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .serializers import ExpenseSerializer
 from .models import Expense
-
+from .serializers import User, UserSerializer
+from django.contrib.auth import authenticate, login, logout
 
 class TrackExpense(APIView):
 
     def post(self, request):
-        title = request.data.get('title')
-        amount = request.data.get('amount')
-        #obj = Expense(user=request.user, title=title, expense=expense)
-        obj = Expense(title=title, amount=amount)
+        title = request.data['title']
+        amount = request.data['amount']
+        obj = Expense(user=request.user, title=title, amount=amount)
+        #obj = Expense(title=title, amount=amount)
         obj.save()
-
-        #expenses = request.objects.filter(user = request.user).order_by('date')
-        expenses = Expense.objects.all().order_by('date')
+        expenses = Expense.objects.filter(user = request.user).order_by('date')
+        #expenses = Expense.objects.all().order_by('date')
         serializer = ExpenseSerializer(expenses, many=True)
         return Response({
-            'status' : '200',
+            'status' : status.HTTP_201_CREATED,
             'data' : serializer.data
         })        
     
     def get(self, request):
-        #expenses = request.objects.filter(user = request.user).order_by('date')
-        expenses = Expense.objects.all().order_by('date')
-        serializer = ExpenseSerializer(expenses)
+        try:
+            expenses = Expense.objects.filter(user = request.user).order_by('date')
+            #expenses = Expense.objects.all().order_by('date')
+            serializer = ExpenseSerializer(expenses, many=True)
+            return Response({
+                'status' : '200',
+                'data' : serializer.data
+            })
+        except Exception as ex:
+            return Response({
+                'status' : status.HTTP_404_NOT_FOUND,
+                'message' : 'No data available.'
+            })
+        
+    def delete(self, request):
+        srno = request.data.get('srno')
+        obj = Expense.objects.get(pk=srno, user=request.user)
+        obj.delete()
         return Response({
-            'status' : '200',
-            'data' : serializer.data
-        }) 
+            'status' : status.HTTP_200_OK,
+            'message' : 'Record deleted successfully.'
+        })
+    
+
+class SignUpView(APIView):
+    def post(self, request):
+
+        username = User.objects.filter(username = request.data.get('username'))
+        if username.exists():
+            return Response({'message' : 'Username Already Exists.'})
+        
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            obj = serializer.save()
+            if obj:
+                return Response({
+                    'status' : status.HTTP_201_CREATED,
+                    'message' : 'Signed Up Successfully.',
+                    'data' : serializer.data
+                })
+
+        return Response({
+            'status' : status.HTTP_400_BAD_REQUEST,
+            'Error' : serializer.errors
+            })
+
+
+class SignInView(APIView):
+    def post(self,request):
+        username = request.data['username']
+        password = request.data['password']
+        user_obj = User.objects.filter(username = username)
+        if not user_obj.exists():
+            return Response({'message' : 'Username Does Not Exists.'})
+        
+        user = authenticate(username = username, password = password)
+        if user:
+            login(request, user)
+            return Response({
+                'status' : status.HTTP_200_OK,
+                'message' : 'Signed In Successfully.'
+            })
+        
+        return Response({
+            'status' : status.HTTP_401_UNAUTHORIZED,
+            'error' : 'Invalid Credentials.'
+        })
+    
+
+class SignOutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({
+            'status': status.HTTP_200_OK,
+            'message' : 'Signed Out Successfully.'
+        })
